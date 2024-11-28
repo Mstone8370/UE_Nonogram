e69748e65848c14576ca4dd03920c24f2e300e46
mstone8370 <mstone8370@gmail.com>
2024-11-28 23:05:49 +0900

    Improve ImageMaker

--- Source/Nonogram/ImageMaker.cpp
+++ Source/Nonogram/ImageMaker.cpp
@@ -2,21 +2,26 @@
 
 
 #include "ImageMaker.h"
+#include "Misc/Paths.h"
+
+const uint32 UImageMaker::FILE_HEADER_SIZE = 14;
+const uint32 UImageMaker::DIB_HEADER_SIZE  = 40;
+const uint8  UImageMaker::FILE_SIZE_OFFSET = 2;
+const uint8  UImageMaker::OFF_BITS_OFFSET  = 10;
+const uint32 UImageMaker::COLOR_TABLE_SIZE = 256 * 4;
+const uint16 UImageMaker::COLOR_PLANE      = 1;
+const uint16 UImageMaker::COLOR_DEPTH      = 8;
+const uint8  UImageMaker::EMPTY_COLOR      = 255;
 
 UImageMaker::UImageMaker()
-    :
-    ImgFolderName(TEXT("img")),
-    InProgressFolderName(TEXT("InProgress")),
-    bIsImgDirValid(false),
-    bIsInProgressDirValid(false)
+    : ImgFolderName(TEXT("img"))
+    , InProgressFolderName(TEXT("InProgress"))
+    , SavedDir(FPaths::ProjectSavedDir())
+    , ImgDir(FPaths::Combine(SavedDir, ImgFolderName))
+    , InProgressDir(FPaths::Combine(ImgDir, InProgressFolderName))
+    , bIsImgDirValid(false)
+    , bIsInProgressDirValid(false)
 {
-    SavedDir = FPaths::ProjectSavedDir();
-    
-    ImgDir = SavedDir;
-    ImgDir.PathAppend(*ImgFolderName, ImgFolderName.Len());
-    InProgressDir = ImgDir;
-    InProgressDir.PathAppend(*InProgressFolderName, InProgressFolderName.Len());
-
     CreateFolders();
 }
 
@@ -30,100 +35,112 @@ void UImageMaker::CreateFolders()
 
 void UImageMaker::Write2Byte(uint16 Value, uint8* File, int32 Offset) const
 {
-    File[Offset] = uint8(Value);
-    File[Offset + 1] = uint8(Value >> 8);
+    File[Offset] = static_cast<uint8>(Value);
+    File[Offset + 1] = static_cast<uint8>(Value >> 8);
 }
 
 void UImageMaker::Write4Byte(uint32 Value, uint8* File, int32 Offset) const
 {
-    Write2Byte(uint16(Value), File, Offset);
-    Write2Byte(uint16(Value >> 16), File, Offset + 2);
+    Write2Byte(static_cast<uint16>(Value), File, Offset);
+    Write2Byte(static_cast<uint16>(Value >> 16), File, Offset + 2);
 }
 
-bool UImageMaker::SaveInProgressImage(FString& Data, FString& FolderName, FString& BoardName, int32 RowSize, int32 ColSize, int32 Color) const
+void UImageMaker::DecodeAndFillPixelData(const FString& Data, uint8* FileData, int32 Color, uint32 FileSize, uint32 RowBytes) const
 {
-    IPlatformFile& FileManager = FPlatformFileManager::Get().GetPlatformFile();
+    const uint8 FilledColor = (uint8)Color;
+    uint8 CurrentColor = EMPTY_COLOR;
 
-    FString FileDir = InProgressDir;
-    FileDir.PathAppend(*FolderName, FolderName.Len());
-    FileManager.CreateDirectory(*FileDir);
-    FileDir.PathAppend(*BoardName, BoardName.Len());
-    FileDir.Append(".bmp");
-
-    uint32 Width = ColSize;
-    uint32 Height = RowSize;
-    // Bitmap의 픽셀 정보에서 가로 줄의 바이트는 4의 배수로 만들어야 함.
-    uint32 HorizontalLength = FMath::CeilToInt32(Width / 4.f) * 4;
-
-    // Bitmap file header size + DIB header size + Color table size = 14 + 40 + 256 * 4 = 1078
-    uint32 PixelStorageOffset = 14 + 40 + 256 * 4;
-    uint32 FileSize = PixelStorageOffset + HorizontalLength * Height;
-
-    uint8* FileData = new uint8[FileSize];
-    memset(FileData, 0, sizeof(uint8) * FileSize);
-
-    // Bitmap file header
-    FileData[0] = 'B';
-    FileData[1] = 'M';
-    Write4Byte(FileSize, FileData, 2);             // File size
-    Write4Byte(PixelStorageOffset, FileData, 10);  // Pixel storage offset
-
-    // DIB header
-    Write4Byte(40, FileData, 14);                  // DIB header size
-    Write4Byte(Width, FileData, 18);               // Width
-    Write4Byte(Height, FileData, 22);              // Hegith
-    Write2Byte(1, FileData, 26);                   // 1 color plane
-    Write2Byte(8, FileData, 28);                   // 8 bit color
-
-    // Color table
-    // 00 00 00 00 ~ FF FF FF 00
-    for (int32 ColorTable = 0; ColorTable < 256; ColorTable++)
-    {
-        // 54 = Color table offset
-        FileData[54 + ColorTable * 4 + 0] = ColorTable;  // B
-        FileData[54 + ColorTable * 4 + 1] = ColorTable;  // G
-        FileData[54 + ColorTable * 4 + 2] = ColorTable;  // R
-    }
-
-    // Pixel storage
     int32 x = 0;
     int32 y = 0;
-
-    uint8 FilledColor = (uint8)Color;
-    uint8 EmptyColor = 255;
-    uint8 CurrentColor = EmptyColor;
     for (const TCHAR& c : Data)
     {
         if (c == '|')
         {
             x = 0;
-            y++;
+            ++y;
             continue;
         }
 
         if ('0' <= c && c < '4')
         {
-            CurrentColor = (c == '1') ? FilledColor : EmptyColor;
+            CurrentColor = (c == '1') ? FilledColor : EMPTY_COLOR;
         }
         else
         {
             const int32 Length = UNonogramStatics::CharToNum(c);
             for (int32 i = 0; i < Length; i++)
             {
-                int32 Offset = FileSize - ((y + 1) * HorizontalLength) + x;
+                // 아래에서부터 위로 픽셀 저장
+                int32 Offset = FileSize - ((y + 1) * RowBytes) + x;
                 FileData[Offset] = CurrentColor;
-                x++;
+                ++x;
             }
         }
     }
+}
 
+bool UImageMaker::SaveInProgressImage(const FString& Data, const FString& FolderName, const FString& BoardName, int32 RowSize, int32 ColSize, int32 Color) const
+{
+    // 파일 시스템 접근을 위한 파일 매니저
+    IPlatformFile& FileManager = FPlatformFileManager::Get().GetPlatformFile();
+
+    // 파일 경로
+    const FString FolderDir = FPaths::Combine(InProgressDir, FolderName);
+    const FString FileName = BoardName + ".bmp";
+    const FString FileDir = FPaths::Combine(FolderDir, BoardName + ".bmp");
+    
+    // 폴더 생성
+    FileManager.CreateDirectory(*FolderDir);
+
+    // 이미지 크기 및 파일 크기 계산
+    const uint32 Width = ColSize;
+    const uint32 Height = RowSize;
+    const uint32 RowBytes = FMath::CeilToInt32(Width / 4.f) * 4; // 4의 배수로
+    const uint32 PixelStorageOffset = FILE_HEADER_SIZE + DIB_HEADER_SIZE + COLOR_TABLE_SIZE;
+    const uint32 FileSize = PixelStorageOffset + (RowBytes * Height);
+
+    // 파일 데이터 메모리 할당
+    TUniquePtr<uint8[]> FileData = MakeUnique<uint8[]>(FileSize);
+    FMemory::Memzero(FileData.Get(), FileSize);
+
+    // Bitmap file header
+    FileData[0] = 'B';
+    FileData[1] = 'M';
+    Write4Byte(FileSize, FileData.Get(), FILE_SIZE_OFFSET);           // File size
+    Write4Byte(PixelStorageOffset, FileData.Get(), OFF_BITS_OFFSET);  // Pixel storage offset
+
+    // DIB header
+    Write4Byte(DIB_HEADER_SIZE, FileData.Get(), FILE_HEADER_SIZE);                  // DIB header size
+    Write4Byte(Width,           FileData.Get(), FILE_HEADER_SIZE + (4 * 1));        // Width
+    Write4Byte(Height,          FileData.Get(), FILE_HEADER_SIZE + (4 * 2));        // Hegith
+    Write2Byte(COLOR_PLANE,     FileData.Get(), FILE_HEADER_SIZE + (4 * 3));        // 1 color plane
+    Write2Byte(COLOR_DEPTH,     FileData.Get(), FILE_HEADER_SIZE + (4 * 3) + 2);    // 8 bit color
+
+    // Color table
+    // 00 00 00 00 ~ FF FF FF 00 (Gray Scale)
+    for (int32 ColorIdx = 0; ColorIdx < 256; ColorIdx++)
+    {
+        const SIZE_T ColorTableOffset = FILE_HEADER_SIZE + DIB_HEADER_SIZE + (ColorIdx * 4);
+        FileData[ColorTableOffset + 0] = ColorIdx;  // Blue
+        FileData[ColorTableOffset + 1] = ColorIdx;  // Green
+        FileData[ColorTableOffset + 2] = ColorIdx;  // Red
+    }
+
+    // Pixel storage
+    DecodeAndFillPixelData(Data, FileData.Get(), Color, FileSize, RowBytes);
+
+    // Save image file
     bool bSuccess = false;
     IFileHandle* FileHandle = FileManager.OpenWrite(*FileDir, false, true);
     if (FileHandle)
     {
-        bSuccess = FileHandle->Write(FileData, sizeof(uint8) * FileSize);
+        bSuccess = FileHandle->Write(FileData.Get(), sizeof(uint8) * FileSize);
         delete FileHandle;  // 파일을 닫기 위해 IFileHandle을 delete
     }
-    delete[] FileData;
+
+    if (!bSuccess)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to save image: %s"), *FileDir);
+    }
     return bSuccess;
 }
--- Source/Nonogram/ImageMaker.h
+++ Source/Nonogram/ImageMaker.h
@@ -19,21 +19,33 @@ class NONOGRAM_API UImageMaker : public UObject
 public:
 	UImageMaker();
 
-protected:
+private:
+	FString ImgFolderName;
+	FString InProgressFolderName;
+
 	FString SavedDir;
 	FString ImgDir;
 	FString InProgressDir;
-
-	FString ImgFolderName;
-	FString InProgressFolderName;
 
 	bool bIsImgDirValid;
 	bool bIsInProgressDirValid;
 
 	void CreateFolders();
+
 	void Write2Byte(uint16 Value, uint8* File, int32 Offset) const;
 	void Write4Byte(uint32 Value, uint8* File, int32 Offset) const;
 
+	void DecodeAndFillPixelData(const FString& Data, uint8* FileData, int32 Color, uint32 FileSize, uint32 RowBytes) const;
+
+	static const uint32 FILE_HEADER_SIZE;
+	static const uint32 DIB_HEADER_SIZE;
+	static const uint8  FILE_SIZE_OFFSET;
+	static const uint8  OFF_BITS_OFFSET;
+	static const uint32 COLOR_TABLE_SIZE;
+	static const uint16 COLOR_PLANE;
+	static const uint16 COLOR_DEPTH;
+	static const uint8  EMPTY_COLOR;
+
 public:
-	bool SaveInProgressImage(FString& Data, FString& FolderName, FString& BoardName, int32 RowSize, int32 ColSize, int32 Color = 255) const;
+	bool SaveInProgressImage(const FString& Data, const FString& FolderName, const FString& BoardName, int32 RowSize, int32 ColSize, int32 Color = 255) const;
 };
